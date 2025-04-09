5126316d8459a7903557a9b8a8015612ab63ae68
Stepan <stepsyota@gmail.com>
2025-04-09 20:36:41 +0300

    Release encryption and split by files

---
+++ BelT.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <cstdint>
+#include <bitset>
+#include <string>
+
+#include "functions.h"
+#include "utils.h"
+
+unsigned int ENCRYPTION();
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "BelT.h"
+
+int main()
+{
+    unsigned int w = 0xC8BA94B1;
+    unsigned int r = ENCRYPTION();
+    return 0;
+}
