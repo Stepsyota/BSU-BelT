de26627f77f4269cba03e1e6c62e96cbfa0fe4ca
Stepan <stepsyota@gmail.com>
2025-05-05 11:26:06 +0300

    Refresh: realese BelT class, improve efficiency, adding key assignment

--- BelT.h
+++ BelT.h
@@ -4,7 +4,21 @@
 #include <bitset>
 #include <string>
 
-#include "functions.h"
-#include "utils.h"
+class BelT {
+	public:
+		BelT(std::string);
+		unsigned int ENCRYPTION();
+	
+	private:
+		unsigned int G_func(unsigned int, unsigned int);
+		unsigned int ShLo(unsigned int);
+		unsigned int ShHi(unsigned int);
+		unsigned int RotHi(unsigned int);
+		unsigned int H_func(unsigned int);
+		uint8_t* Split32to8(unsigned int word);
+		unsigned int Connect8to32(uint8_t parts[]);
+		unsigned int WordToNum(unsigned int word);
+		unsigned int NumToWord(unsigned int word);
 
-unsigned int ENCRYPTION();
+		unsigned int ROUND_KEY[56]{};
+};
--- utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <cstdint>
-
-uint8_t* Split32to8(unsigned int word);
-unsigned int Connect8to32(uint8_t parts[]);
-unsigned int WordToNum(unsigned int word);
-unsigned int NumToWord(unsigned int word);
